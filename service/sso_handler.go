e716dbfc2bed8f0218147714392ccdacf90693e0
napoleon <naphaluan211092@gmail.com>
2021-04-02 13:46:56 +0700

    Fix code

--- service/sso_handler.go
+++ service/sso_handler.go
@@ -13,13 +13,13 @@ func ParseSSOToken(c echo.Context) error {
 	err := c.Bind(authenRequestBody)
 	if err != nil {
 		fmt.Println(err)
-		return c.JSON(400, map[string]interface{}{"code": "6", "message": "Body is Invalid", "data": map[string]interface{}{"info": nil}})
+		return c.JSON(400, map[string]interface{}{"code": "6", "message": "Body is Invalid", "data": nil})
 	}
 	token := authenRequestBody.Token
 	authResponse, err := BasicAuth(token)
 	//
 	if err != nil {
-		return c.JSON(200, map[string]interface{}{"code": "10", "message": "Connection refused", "data": map[string]interface{}{"info": nil}})
+		return c.JSON(200, map[string]interface{}{"code": "10", "message": "Connection refused", "data": nil})
 	}
 	//
 	var result map[string]interface{}
@@ -27,12 +27,13 @@ func ParseSSOToken(c echo.Context) error {
 
 	if result["error"] != nil {
 		if result["message"] != nil {
-			return c.JSON(200, map[string]interface{}{"code": "7", "message": (result["message"]).(string), "data": map[string]interface{}{"info": nil}})
+			return c.JSON(200, map[string]interface{}{"code": "7", "message": (result["message"]).(string), "data": nil})
 		}
-		return c.JSON(200, map[string]interface{}{"code": "5", "message": (result["error"]).(string), "data": map[string]interface{}{"info": nil}})
+		return c.JSON(200, map[string]interface{}{"code": "5", "message": (result["error"]).(string), "data": nil})
 	}
 	//test
 	attributes := result["attributes"].(map[string]interface{})
 	attributes["username"] = result["id"]
-	return c.JSON(200, map[string]interface{}{"code": "0", "message": "success", "data": map[string]interface{}{"info": attributes}})
+	//return c.JSON(200, map[string]interface{}{"code": "0", "message": "success", "data": map[string]interface{}{"info": attributes}})
+	return c.JSON(200, map[string]interface{}{"code": "0", "message": "success", "data": attributes})
 }
